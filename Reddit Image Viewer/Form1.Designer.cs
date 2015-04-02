f7107bd6549d068bb5e18253e2c3af8339cd5175
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-04-02 18:50:46 +0200

    gets the newest images from reddit frontpage

--- Reddit Image Viewer/Form1.Designer.cs
+++ Reddit Image Viewer/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 381);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Prev";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 65);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.label1.Location = new System.Drawing.Point(118, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(529, 476);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Reddit Image Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Reddit Image Viewer/Form1.cs
+++ Reddit Image Viewer/Form1.cs
@@ -4,17 +4,98 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace Reddit_Image_Viewer
 {
     public partial class Form1 : Form
     {
+        public dynamic GetJSON()
+        {
+            WebClient client = new WebClient();
+            dynamic JSONResult;
+
+            try
+            {
+                JSONResult = JsonConvert.DeserializeObject(client.DownloadString("http://reddit.com/.json"));
+
+                return JSONResult;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+
+                return (string) e.Message;
+            }
+        }
+        public Dictionary<string, string> ParseJSON(dynamic _jsoncontent)
+        {
+            Dictionary<string, string> Result = new Dictionary<string, string>();
+
+            List<string> AcceptedDomains = new List<string>();
+            AcceptedDomains.Add("imgur.com");
+            AcceptedDomains.Add("i.imgur.com");
+
+            foreach (dynamic _child in _jsoncontent.data.children) 
+            {
+                string _url, _title;
+
+                try
+                {
+                    _url = (string)_child.data.url;
+                    _title = (string)_child.data.title;
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+
+                if (AcceptedDomains.Contains((string)_child.data.domain) && (_url.Contains(".jpg") || _url.Contains(".png")))
+                {
+                    Result.Add(_url, _title);
+                }
+            }
+
+            foreach (var _keyval in Result) 
+            {
+                Console.WriteLine("{0} ({1})", _keyval.Value, _keyval.Key);
+            }
+
+            return Result;
+        }
         public Form1()
         {
             InitializeComponent();
+            dynamic JSON = GetJSON();
+            Dictionary<string, string> JSONResult = ParseJSON(JSON);
+
+            Console.Write("");
+
+            // pictureBox1.Load();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // Prev image
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // Next image
+        {
+
         }
     }
 }
